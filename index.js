46dd88f5c6272408c04ae3d873a6d1742be3f3c1
HristiyanZahariev <ihzahariev@gmail.com>
2017-03-22 00:22:57 +0200

    Tring to set it up

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
+// var server = app.listen(8081, function () {
+//    var host = server.address().address
+//    var port = server.address().port
    
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+//    console.log("Example app listening at http://%s:%s", host, port)
+// })
