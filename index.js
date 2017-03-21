d4d047fb0d48ff40be8f3e7d6e66327cc99ee14f
HristiyanZahariev <ihzahariev@gmail.com>
2017-03-22 00:45:34 +0200

    yet again

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
-// var server = app.listen(8081, function () {
-//    var host = server.address().address
-//    var port = server.address().port
-   
-//    console.log("Example app listening at http://%s:%s", host, port)
-// })
+var port = process.env.PORT || 8000
+
+server.listen(port, function() {
+    console.log("App is running on port " + port);
+});
