2d06764a287ca9987115bad739d7cc13d98b14dd
HristiyanZahariev <ihzahariev@gmail.com>
2017-03-22 10:42:20 +0200

    oooopppps

--- index.js
+++ index.js
@@ -13,16 +13,6 @@ var connection = mysql.createConnection(
 	database : "heroku_2ddbb6dcb252ea7"
 )
 
-app.get('/', function(request, response) {
-    connection.query('SELECT * from users', function(err, rows, fields) {
-        if (err) {
-            console.log('error: ', err);
-            throw err;
-        }
-        response.send(['Hello World!!!! HOLA MUNDO!!!!', rows]);
-    });
-});
-
 
 var port = process.env.PORT || 8000
 
@@ -32,3 +22,13 @@ var server =  app.listen(port, function () {
    
    console.log("Example app listening at http://%s:%s", host, port)
 })
+
+app.get('/', function(request, response) {
+    connection.query('SELECT * from users', function(err, rows, fields) {
+        if (err) {
+            console.log('error: ', err);
+            throw err;
+        }
+        response.send(['Hello World!!!! HOLA MUNDO!!!!', rows]);
+    });
+});
