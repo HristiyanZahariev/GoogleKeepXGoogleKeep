e5ee1c1927ca2ccde94ef958a22450d405f983f5
emil <emilfrogy@gmail.com>
2017-04-09 01:00:45 +0300

    some files

--- api/models/user.js
+++ api/models/user.js
@@ -1,38 +1,51 @@
-var indexFile = require('../index.js');
-var sequalize = indexFile.sequalize;
+"use strict";
 
-console.log(indexFile.sequalize);
+module.exports = function(sequelize, DataTypes){
+
+  var User = sequelize.define('users', {
+    id: {
+     type: DataTypes.BIGINT(11),
+     autoIncrement: true,
+     primaryKey: true,
+     field: 'id'
+    },
+    username: {
+      type: DataTypes.STRING(),
+      field: 'username' 
+    },
+    password: {
+      type: DataTypes.STRING(),
+      field: 'password'
+    },
+    email: {
+     type: DataTypes.STRING(),
+     field: 'email'
+    },
+    firstName: {
+     type: DataTypes.STRING(),
+     field: 'first_name'
+    },
+    lastName: {
+     type: DataTypes.STRING(),
+     field: 'last_name'
+    }
+  }, {
+    freezeTableName: true // Model tableName will be the same as the model name
+  },{
+    classMethods: {
+        associate: function(models) {
+            User.hasMany(models.Notes, {through: 'user_notes'});
+        }
+    },
+    instanceMethods: {
+      retrieveAll: function(onSuccess, onError) {
+         User.findAll().then(onSuccess).error(onError);
+      }
+   }
 
 
-var User = sequelize.define('user', {
-  id: {
-  	type: Sequelize.BIGINT(11),
-  	autoIncrement: true,
-  	primaryKey: true,
-  	field: 'id'
-  },
-  username: {
-    type: Sequelize.STRING,
-    field: 'username' 
-  },
-  password: {
-    type: Sequelize.STRING,
-    field: 'password'
-  },
-  email: {
-  	type: Sequelize.STRING,
-  	field: 'email'
-  },
-  firstName: {
-  	type: Sequelize.STRING,
-  	field: 'first_name'
-  },
-  lastName: {
-  	type: Sequelize.STRING,
-  	field: 'last_name';
-  }
-}, {
-  freezeTableName: true // Model tableName will be the same as the model name
-});
 
-module.exports = sequalize.model('User', User);
+  });
+
+  return User;
+};
