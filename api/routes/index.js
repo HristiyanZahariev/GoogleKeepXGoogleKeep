449865bc820ff50eb9bd7d5d5e2626af2bcc6a6c
HristiyanZahariev <ihzahariev@gmail.com>
2017-05-01 04:14:51 +0300

    d

--- api/routes/index.js
+++ api/routes/index.js
@@ -95,7 +95,7 @@ router.get('/auth/twitter',
 router.get('/auth/twitter/callback', 
   passport.authenticate('twitter', { failureRedirect: '/login' }),
   function(req, res) {
-      var payload = {id: req.user};
+      var payload = {id: req.user.twitterId};
       var token = jwt.sign(payload, cfg.jwtSecret);
       res.json({message: "ok", token: token});
       console.log(req.user)
